759ce498b0ca7dc8c7516e16406954d35274dbe1
jesusjanm <janmjesus@gmail.com>
2019-10-29 13:33:53 -0400

    Añadiendo caracteristicas de ruteo

--- platzi-square/src/app/detalle/detalle.component.ts
+++ platzi-square/src/app/detalle/detalle.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-detalle',
@@ -6,6 +7,22 @@ import { Component } from '@angular/core';
 
 })
 export class DetalleComponent {
-  title = 'Detall';
+
+    hola = "hola";
+  //let idRecibido = "";
+    idRecibido = "";
+
+    constructor( private route: ActivatedRoute ){
+
+      //console.log("EPAAAAAAAAA");
+     //console.log( this.route.snapshot.params['id'] );
+
+      this.hola = "hola2";
+      this.idRecibido = this.route.snapshot.params['id'];
+      // idRecibido = this.route.snapshot.params['id'];
+
+
+
+     }
 
 }
--- platzi-square/src/app/lugares/lugares.component.ts
+++ platzi-square/src/app/lugares/lugares.component.ts
@@ -9,10 +9,10 @@ export class LugaresComponent {
   title = 'platzi-square';
 
   lugares = [
-    {plan: 'pagado',cercania: 1, distancia: 1, nombre: "Nombre1", active: 0},
-    {plan: 'gratuito',cercania: 3, distancia: 15, nombre: "Nombre2", active: 1},
-    {plan: 'gratuito',cercania: 2, distancia: 5, nombre: "Nombre3", active: 0},
-    {plan: 'gratuito',cercania: 3, distancia: 15, nombre: "Nombre5", active: 1},
+    {id:1,plan: 'pagado',cercania: 1, distancia: 1, nombre: "Nombre1", active: 0},
+    {id:2,plan: 'gratuito',cercania: 3, distancia: 15, nombre: "Nombre2", active: 1},
+    {id:3,plan: 'gratuito',cercania: 2, distancia: 5, nombre: "Nombre3", active: 0},
+    {id:4,plan: 'gratuito',cercania: 3, distancia: 15, nombre: "Nombre5", active: 1},
   ];
 
   lat = 0;
